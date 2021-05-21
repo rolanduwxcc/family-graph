a712ddc22af54f9abed5c8396b62f6247eba9506
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-05-20 20:50:59 -0500

    adjusted some server file structure

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/family-graph', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: true,
+  useFindAndModify: false,
 //not 100% these are the right settings we need for true/false, just taking from book search for now
 });
 
