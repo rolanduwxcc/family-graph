15d9b5dbd9138d2d1487d10fad8e17ea086c2978
Smack <kovacsmyles1@gmail.com>
2021-05-23 22:08:41 -0500

    unit page pointing to unitCard, struggling to pull the unit data

--- client/src/components/UnitCard/index.js
+++ client/src/components/UnitCard/index.js
@@ -7,14 +7,14 @@ function unitCard() {
         <div class="card-body">
           <h5 class="card-title">{unit.firstName, unit.Lastname}</h5>
           <div class="card-text-container">
-            <p>{firstName}</p>
-            <p>{lastName}</p>
-            <p>{otherNames}</p>
-            <p>{mother}</p>
-            <p>{father}</p>
-            <p>{children}</p>
-            <p>{birthCountry}</p>
-            <p>{homeCountry}</p>
+            <p>{unit.firstName}</p>
+            <p>{unit.lastName}</p>
+            <p>{unit.otherNames}</p>
+            <p>{unit.mother}</p>
+            <p>{unit.father}</p>
+            <p>{unit.children}</p>
+            <p>{unit.birthCountry}</p>
+            <p>{unit.homeCountry}</p>
           </div>
           <a href="#" class="btn btn-primary">
             Add Parent
